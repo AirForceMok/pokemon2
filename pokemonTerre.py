8c789512bbcba03fa22105449e76060b408387a7
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-07 11:56:39 +0200

    Create pokemonTerre.py

---
+++ pokemonTerre.py
@@ -0,0 +1,9 @@
+from pokemon import pokemon
+
+class pokemonTerre(pokemon)
+
+    def __init__(self, nom: str, poids: float , taille : float, nb_Pattes : int):
+        self.__nom = nom
+        self.__poids = poids
+        self.__nbPattes = nbPattes
+        self.__taille = taille
