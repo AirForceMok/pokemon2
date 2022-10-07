c5ebceae4ee723d549431f0c4fb11df49f1e6a93
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-07 11:56:13 +0200

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,7 +1,22 @@
+from abc import ABC, abstractmethod
 
-class pokemon
+class pokemon(ABCà)
 
     def __init__(self, nom: str, poids: float):
         self.__nom = nom
         self.__poids = poids
 
+    def __str__(self): -> str:
+        return f"Je suis le pokemon de {self.__nom}, mon poids est de {self.__poids}"
+
+    abstractmethod()
+    def vitesse(self) -> float:
+        pass
+    
+    @property
+    def nom(self):
+            return self.__nom
+    
+    @property
+    def poids(self):
+            return self.__poids
