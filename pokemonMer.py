f69ae5cdbca0586b1216914d1d2f1a5c5060ac8d
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-07 11:58:05 +0200

    Create pokemonMer.py

---
+++ pokemonMer.py
@@ -0,0 +1,7 @@
+from pokemonEau import pokemonEau
+class pokemonMer(pokemonEau)
+    def __init__(self, nom: str, poids: float, nbNageoires: int):
+        self.__nom = nom
+        self.__poids = poids
+        self.__nbNageoires = nbNageoires
+
