a54de31ba7f9841470dc87b5e1b519b6540a6657
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-13 15:00:34 +0200

    Update pokemonCroisiere.py

--- pokemonCroisiere.py
+++ pokemonCroisiere.py
@@ -1,6 +1,28 @@
 from pokemonEau import pokemonEau
 class pokemonCroisiere(pokemonEau)
-    def __init__(self, nom: str, poids: float, nbNageoires: int):
+    def __init__(self, nom: str, poids: float, nbNageoires):
         self.__nom = nom
-        self.__poids = poids
-        self.__nbNageoires = nbNageoires
+
+    return f"Je suis le pokémon {self.__nom} mon poids est de {self.__poids} "
+    ma
+    vitesse
+    est
+    de
+    {self.__vitesse}
+    " \
+                 f "
+    je
+    possède
+    {self.__nbNageoires}
+    nageoires
+    " \
+
+    @poids.setter
+    def poids(self, poids: int):
+        if poids > 0:
+            self.__poids = poids
+
+    @nbNageoires.setter
+    def nbNageoires(self, nbNageoires: int):
+        if nbNageoires > 0:
+            self.__nbNageoires = nbNageoires
