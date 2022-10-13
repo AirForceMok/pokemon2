8ba24f0f654ee18eeaf300e565adddf799ced966
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-13 15:01:11 +0200

    Update pokemonMer.py

--- pokemonMer.py
+++ pokemonMer.py
@@ -1,7 +1,19 @@
 from pokemonEau import pokemonEau
 class pokemonMer(pokemonEau)
-    def __init__(self, nom: str, poids: float, nbNageoires: int):
-        self.__nom = nom
-        self.__poids = poids
-        self.__nbNageoires = nbNageoires
 
+    def __init__(self, nom: str, poids: float , nbNageoires, vitesse : float):
+        self.__nom = nom
+        self.__vitesse = vitesse
+
+     return f"Je suis le pokémon {self.__nom} mon poids est de {self.__poids} "ma vitesse est de {self.__vitesse}" \
+               f "je possède {self.__nbNageoires} nageoires" \
+
+    @poids.setter
+    def poids(self, poids: int):
+        if poids > 0:
+            self.__poids = poids
+
+    @nbNageoires.setter
+    def nbNageoires(self, nbNageoires: int):
+        if nbNageoires > 0:
+            self.__nbNageoires = nbNageoires
