017279a097d0d657acd713ce579d216af566115a
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-13 15:00:18 +0200

    Update pokemonCasanier.py

--- pokemonCasanier.py
+++ pokemonCasanier.py
@@ -1,9 +1,20 @@
 from pokemonTerre import pokemonTerre
 class pokemonCasanier(pokemonTerre)
-    def __init__(self, nom: str, poids: float, nbPattes: int, taille: float, nbHeures: int):
-        self.__nom = nom
-        self.__poids = poids
-        self.__nbPattes = nbPattes
-        self.__taille = taille
+    def __init__(self, nom: str, poids: float, taille: float, nbPattes: int, vitesse: float, nbHeures : float):
+        self.__vitesse = vitesse
         self.__nbHeures = nbHeures
 
+return f"Je suis le pokémon {self.__nom} mon poids est de {self.__poids} ma vitesse est de {self.__vitesse}" \
+               f" je possède {self.__nbPattes} pattes, ma taille est de {self.__taille}" \
+
+    def vitesse(self) -> float:
+        pass
+
+
+    @property
+    def nom(self):
+    return self.__nom
+
+    @property
+    def poids(self):
+    return self.__poids
