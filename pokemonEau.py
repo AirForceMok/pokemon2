3d12a4818f9370baae44a15fc52049c1f72bc4ed
denisbui <64400694+denisbui@users.noreply.github.com>
2022-10-13 15:00:47 +0200

    Update pokemonEau.py

--- pokemonEau.py
+++ pokemonEau.py
@@ -1,4 +1,15 @@
 from pokemon import pokemon
 class pokemonEau(pokemon)
-    def __init__(self, nbNageoires: int):
+    def __int__(self, nom: str, poids: float , nbNageoires):
     self.__nbNageoires = nbNageoires
+
+    def vitesse(self) -> float:
+        pass
+
+    @property
+    def nom(self):
+        return self.__nom
+
+    @property
+    def poids(self):
+        return self.__poids
